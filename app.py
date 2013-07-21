7317d0f09eabb07081bdccf5602b7d2538fcc31c
Kelvin Law <kelvin@twilio.com>
2013-07-20 18:56:18 -0700

    Extract playlists into their own file

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
 from gevent import monkey
+from flask import Flask, render_template
 import os
 
 monkey.patch_all()
 
-from flask import Flask
-from flask import send_from_directory
+class CustomFlask(Flask):
+    jinja_options = Flask.jinja_options.copy()
+    jinja_options.update(dict(
+        block_start_string='<%',
+        block_end_string='%>',
+        variable_start_string='%%',
+        variable_end_string='%%',
+        comment_start_string='<#',
+        comment_end_string='#>',
+    ))
 
-app = Flask(__name__, static_folder='static')  
+app = CustomFlask(__name__, static_folder='static')  
 
-@app.route('/<path:filename>')  
-def send_file(filename):  
-    return send_from_directory(app.static_folder, filename)
+playlists = '"{}"'.format('", "'.join([x for x in open("playlists.txt").read().split("\n")]))
+
+google_api_key = "AIzaSyDkzz9NxYT0beSLWdd9m8a5-lEOhBZj71M"
+
+@app.route("/")
+def index():
+	return render_template('index.html', google_api_key=google_api_key, playlists=playlists)
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
